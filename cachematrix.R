c0d0c5821f12fb401d7604f2bf3b8c62ca06a156
anstariq <anstariq2297@gmail.com>
2020-08-30 07:12:14 +0500

    assignment 2 final

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,8 @@ makeVector <- function(x = numeric()) {
         list(set = set, get = get,
              setInverse = setInverse,
              getInverse = getInverse)
+        ##The sole purpose of this function is to generate a matrix and display it. Meanwhile, this function also calls the second function to get the inverse. 
+        
 }
 
 
@@ -26,4 +28,5 @@ cacheSolve <- function(x, ...) {
         inv <- solve(data, ...)
         x$setInverse(inv)
         inv
+        ## This function is supposed to check the cache whether the inverse is already present or not.If it is present, then it displays the message that it is fetching cache details.Otherwise it processes and generates the inverse.
 }
